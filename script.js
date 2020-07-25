b47c5f00a2d767f8a970bb33ea32d8e867b9473e
mahi <mahiphy00@gmail.com>
2020-07-25 18:53:54 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  var apikey = "149670aa";
+  $("#searchForm").on("submit", (e) => {
+    e.preventDefault();
+    var searchText = $("#searchText").val();
+
+    var url = "http://www.omdbapi.com/?apikey=" + apikey;
+
+    $.ajax({
+      method: "GET",
+      url: url + "&s=" + searchText,
+      success: function (data) {
+        console.log(data);
+        let movies = data.Search;
+        console.log(movies);
+        output = "";
+        $.each(movies, (index, movie) => {
+          output += `
+          <div class="col-md-3 col-sm-6">
+          <div class = "well text-center">
+          <img src="${movie.Poster}">
+          <p>${movie.Year}</p>
+          <h5>${movie.Title}</h5>
+          <a class ="btn btn-primary" href="https://www.imdb.com/title/${movie.imdbID}" target="popup">MOVIE DETAILS ON IMDB</a>
+          </div></div>`;
+        });
+
+        $("#movies").html(output);
+      },
+    });
+  });
+});
